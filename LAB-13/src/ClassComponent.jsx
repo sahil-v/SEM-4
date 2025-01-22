ab24f3ab878ac4ce122d06111f98c4a6b05ddca7
sahil-v <vasoyasahil608@gmail.com>
2025-01-22 11:11:04 +0530

    h2 tag change

--- LAB-13/src/ClassComponent.jsx
+++ LAB-13/src/ClassComponent.jsx
@@ -3,7 +3,12 @@ import { Component } from "react";
 class ClassComponent extends Component {
     render() {
         return(
-        <div>This is a class component.</div>
+            <>
+                <div>This is a class component.</div>
+
+                <H2>Helloo</H2>
+            </>
+        
         ) 
     }
 }
